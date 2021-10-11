70a51d8a3ea1b9a80804699e73960da02575fd60
Finley Sherwood <me@itsf.in>
2021-10-11 12:08:58 +1000

    Extend parts from WeaponPart

--- models/items/Scope.ts
+++ models/items/Scope.ts
@@ -1,36 +1,18 @@
-import { Price, Rarity } from '@typings/item'
-import Item from './Item'
+import { WeaponPartData, WeaponPart } from './WeaponPart'
 
-interface ScopeData {
-  rarity?: Rarity
-  price?: Price
-  name: string
+interface ScopeData extends WeaponPartData {
   showsAmmo?: boolean
   infrared?: boolean
-  zoom?: number
-  scopeIn?: number
 }
 
-export default abstract class Scope extends Item {
-  public name: string
+export default abstract class Scope extends WeaponPart {
   public showsAmmo: boolean
   public infrared: boolean
-  public zoom: number
-  public scopeIn: number
 
   constructor(data: ScopeData) {
-    super({
-      rarity: data.rarity ?? 'Uncommon',
-      price: data.price ?? {
-        gp: [120, 5600],
-        zen: [10, 150]
-      }
-    })
+    super(data)
 
-    this.name = data.name
     this.showsAmmo = data.showsAmmo ?? false
     this.infrared = data.infrared ?? false
-    this.zoom = data.zoom ?? 0
-    this.scopeIn = data.scopeIn ?? 0
   }
 }
---
+++ models/items/WeaponPart.ts
@@ -0,0 +1,72 @@
+import { Price, Rarity } from '@typings/item'
+import Item from './Item'
+
+export interface WeaponPartData {
+  rarity?: Rarity
+  price?: Price
+  name: string
+  damage?: number
+  fireRate?: number
+  ammo?: [number, number]
+  reload?: number
+  swapRate?: number
+  spread?: {
+    aim?: number
+    hip?: number
+    move?: number
+  }
+  recoil?: number
+  zoom?: number
+  scopeIn?: number
+  range?: [number, number]
+  run?: number
+  missingData?: boolean // For if wiki is missing info
+}
+
+export class WeaponPart extends Item {
+  public name: string
+  public damage: number
+  public fireRate: number
+  public ammo: [number, number]
+  public reload: number
+  public swapRate: number
+  public spread: {
+    aim: number
+    hip: number
+    move: number
+  }
+  public recoil: number
+  public zoom: number
+  public scopeIn: number
+  public range: [number, number]
+  public run: number
+  public missingData: boolean
+
+  constructor(data: WeaponPartData) {
+    super({
+      rarity: data.rarity ?? 'Common',
+      price: data.price ?? {
+        gp: [0, 0],
+        zen: [0, 0]
+      }
+    })
+
+    this.name = data.name
+    this.damage = data.damage ?? 0
+    this.fireRate = data.fireRate ?? 0
+    this.ammo = data.ammo ?? [0, 0]
+    this.reload = data.reload ?? 0
+    this.swapRate = data.swapRate ?? 0
+    this.spread = {
+      aim: data.spread?.aim ?? 0,
+      hip: data.spread?.hip ?? 0,
+      move: data.spread?.move ?? 0
+    }
+    this.recoil = data.recoil ?? 0
+    this.zoom = data.zoom ?? 0
+    this.scopeIn = data.scopeIn ?? 0
+    this.range = data.range ?? [0, 0]
+    this.run = data.run ?? 0
+    this.missingData = data.missingData ?? false
+  }
+}
