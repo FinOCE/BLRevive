e496453c80eef330cd5b10bb933bde296f9204db
Finley Sherwood <me@itsf.in>
2021-11-13 14:22:24 +1000

    Start creating UI

---
+++ ui/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Item />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Item from './components/Item.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Item
+  }
+})
+</script>
+
+<style lang="sass">
+body
+  background: #000
+</style>
